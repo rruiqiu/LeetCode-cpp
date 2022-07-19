55535b081aed56ac1cfe6a5449ed1d60d524cbd3
richardd <42120484+richardddddddd@users.noreply.github.com>
2022-07-18 21:04:16 -0400

    Create sorting.cpp

---
+++ data-structure/sorting.cpp
@@ -0,0 +1,13 @@
+// sort algorithm example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::sort
+#include <vector>       // std::vector
+
+int main(){
+  int myints[] = {32,71,12,45,26,80,53,33};
+  std::vector<int> myvector (myints, myints+8);               // 32 71 12 45 26 80 53 33
+
+  // using default comparison (operator <):
+  std::sort (myvector.begin(), myvector.begin()+4);           //(12 32 45 71)26 80 53 33
+	
+}
