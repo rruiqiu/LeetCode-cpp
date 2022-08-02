5aa34f280c7338da5fd4fbf126f237f549365a86
richardd <42120484+richardddddddd@users.noreply.github.com>
2022-08-02 17:35:24 -0400

    Create 0205.Isomorphic Strings.cpp

---
+++ leetcode/0205.Isomorphic Strings/0205.Isomorphic Strings.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+bool isIsomorphic(string X, string Y)
+{
+    // if 'X' and 'Y' have different lengths, they cannot be isomorphic
+    if (X.length() != Y.length()) {
+        return false;
+    }
+ 
+    // use a map to store a mapping from characters of string 'X' to string 'Y'
+    unordered_map<char, char> map;
+ 
+    // use set to store a pool of already mapped characters
+    unordered_set<char> set;
+ 
+    for (int i = 0; i < X.length(); i++)
+    {
+        char x = X[i], y = Y[i];
+ 
+        // if 'x' is seen before
+        if (map.find(x) != map.end())
+        {
+            // return false if the first occurrence of `x` is mapped to a
+            // different character
+            if (map[x] != y) {
+                return false;
+            }
+        }
+ 
+        // if 'x' is seen for the first time (i.e., it isn't mapped yet)
+        else {
+            // return false if 'y' is already mapped to some other char in 'X'
+            if (set.find(y) != set.end()) {
+                return false;
+            }
+ 
+            // map 'y' to 'x' and mark it as mapped
+            map[x] = y;
+            set.insert(y);
+        }
+    }
+ 
+    return true;
+}
+ 
+};
